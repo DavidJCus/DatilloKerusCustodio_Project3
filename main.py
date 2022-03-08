777fba10aa9f85bf7e7389e3fbfb1961a70d30eb
davidcustodio <davidjcustodio@gmail.com>
2022-03-08 18:56:15 -0500

    Started on allowing user to input files.

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 
 root = Tk()
 
-e = Entry(root, width=75)
-e.pack()
+
+def chooseFile():
+    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
+    print("You chose: " + filename)
 
 
-def valueEntry():
-    value = e.get()
-    label = Label(root, text="So this is what you entered: " + value)
-    label.pack()
+def done():
+    # function to do calculations should go here
+    root.destroy()  # if pressed first, then ends whole process
 
 
-button = Button(root, text="Enter", command=valueEntry)
+# this seems to be working
+button = Button(root, text="Select attributes file", command=chooseFile)
 button.pack()
+button2 = Button(root, text="Select the hard constraints", command=chooseFile)
+button2.pack()
+endButton = Button(root, text="Done", command=done)
+endButton.pack(pady=20)
 
 root.mainloop()
+
+"""
+use this to function to get contents of a file into a variable, maybe an array of content? lines[i]
+
+with open(filename) as f:
+    lines = f.readlines()
+"""
