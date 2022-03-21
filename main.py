a06e60c408fa8d2d887ce21934f17fc0b1a95eda
davidcustodio <davidjcustodio@gmail.com>
2022-03-21 17:07:11 -0400

    Contents of user chosen files now are assigned to a list of strings.

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ window.title = "Enter files"
 window.geometry("300x160")
 window.eval('tk::PlaceWindow . center')
 
+files = []
+
 
 def chooseFile():
     Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
     filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
     print("You chose: " + filename)
+    with open(filename) as f:
+        lines = f.readlines()
+    print(lines)
+    files.append(lines)
 
 
 def done():
+    print("You pressed done, here are the results")
+    # print("First file: " + str(files[0]) + " Second File: " + str(files[1]) + " Third File: " + str(files[2]))
     # function to do calculations should go here
     window.destroy()  # if pressed first, then ends whole process
 
