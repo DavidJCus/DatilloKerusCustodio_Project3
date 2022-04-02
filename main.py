e28e973007729514a543b3eeeacc77aaaf97258d
Karli Dattilo <dattilosubscribed97@gmail.com>
2022-04-01 20:28:59 -0400

    main.py: font size label changed

--- main.py
+++ main.py
@@ -556,7 +556,7 @@ myCanvas.create_image(0, 0, image=newBg)
 # add a label to canvas
 myCanvas.create_text(50, 20, text="Constraints", font=("Batang", 11), fill="black")
 myCanvas.create_text(50, 140, text="Preference", font=("Batang", 11), fill="black")
-myCanvas.create_text(64, 305, text="Possible Tasks", font=("Batang", 10), fill="black")
+myCanvas.create_text(64, 305, text="Possible Tasks", font=("Batang", 11), fill="black")
 
 # create images and resize them for buttons
 # attributes
